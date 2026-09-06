b1afbf8eb626d4fd6e8555689c7cb5e266fdf79c
Marko <markopavlo.69@gmail.com>
2026-09-06 16:10:32 +0200

    logs can be added and render

--- expenses.js
+++ expenses.js
@@ -10,5 +10,39 @@ const logs = document.querySelector("#logs");
 let transactions = [];
 
 submitBtn.addEventListener("click", function(){
-    console.log("clicked");
+    transactions.push(
+        {
+            id: Date.now(),
+            amount: amountInput.value,
+            desc: descInput.value,
+            date : dateInput.value,
+            type: typeInput.value,
+            cat: catInput.value
+        }
+    )
+    render();
 })
+
+function render(){
+    logs.innerHTML="";
+    
+    transactions.forEach((transaction) =>{
+        const logList = document.createElement("div");
+        logList.className = "logList"
+       const amountItem = document.createElement("p");
+       amountItem.textContent = `${amountInput.value}€`;
+       const descItem = document.createElement("p");
+       descItem.textContent = descInput.value;
+       const dateItem = document.createElement("p");
+       dateItem.textContent = dateInput.value;
+       const typeItem = document.createElement("p");
+       typeItem.textContent = typeInput.value;
+       const catItem = document.createElement("p");
+       catItem.textContent = catInput.value;
+       logList.append(amountItem,descItem, dateItem, typeItem,catItem);
+       logs.append(logList);
+       
+    })
+}
+
+render();
